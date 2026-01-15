2b389e89ebeb30f0ef33562885aa8066c95bd726
MTCMarketing <slapkommerce@gmail.com>
2026-01-14 19:55:13 -0700

    Create static-config.js

---
+++ static-config.js
@@ -0,0 +1,13 @@
+import {SITE_CONTENT} from "./content-payload.js";
+
+export const MAIN_CONTENT = {
+  UNDER_CONSTRUCTION: false,
+  ENABLE_EDITOR: false,
+  ROADMAP_HIDE_RELATED_KEYWORDS: true,
+  ROADMAP_HIDE_SOCIALS_ON_PROFILE: true,
+  HIDE_CONTACT_ON_PUBLIC_PROFILE: true,
+  HIDE_LANDING_VIDEO_SECTION: false,
+  URL_REWRITING: true,
+  DEFAULT_LANGUAGE: "vn",
+  CONTENT: undefined,
+};
